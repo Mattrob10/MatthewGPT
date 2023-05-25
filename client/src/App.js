68516061761c0ec34fae08c1a9c76642e298cb0c
Matthew Robinson <Mmrobinson1983@gmail.com>
2023-05-25 15:59:33 -0400

    updated response fetch endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -31,10 +31,7 @@ function App() {
       },
     };
     try {
-      const response = await fetch(
-        "http://localhost:8000/completions",
-        options
-      );
+      const response = await fetch("/completions", options);
       if (response.ok) {
         const data = await response.json();
         setMessage(data.choices[0].message);
