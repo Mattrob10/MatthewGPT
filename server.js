4dedb7789021f3c9e41b33e59b9e57606b80d33f
Matthew Robinson <Mmrobinson1983@gmail.com>
2023-05-25 14:25:07 -0400

    updated CORS

--- server.js
+++ server.js
@@ -1,45 +1,49 @@
-const PORT = 8000;
-
 const express = require('express');
 const cors = require('cors');
 require('isomorphic-fetch'); 
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
-app.use(express.json());
-app.use(cors());
-const path = require("path")
+const path = require("path");
 
 const API_KEY = process.env.API_KEY;
 
+app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET, POST",
+  allowedHeaders: "Content-Type",
+}));
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.post('/completions', async (req, res) => {
   const options = {
     method: "POST",
     headers: {
-        "Authorization": `Bearer ${API_KEY}`,
-        "Content-Type": "application/json"
+      "Authorization": `Bearer ${API_KEY}`,
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: req.body.message}],
+      messages: [{ role: "user", content: req.body.message }],
       max_tokens: 100,
-    }) 
+    })
   };
+
   try {
-   const response = await fetch('https://api.openai.com/v1/chat/completions', options);
-   const data = await response.json();
-   console.log(data);
-   res.setHeader('Access-Control-Allow-Origin', '*'); 
-   res.send(data); 
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json();
+    console.log(data);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(data);
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred.");
   }
 });
 
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-app.listen(PORT, ()  => console.log(`Server is running on PORT: ` + PORT));
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
